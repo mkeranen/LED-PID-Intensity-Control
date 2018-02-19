8fb15ab865da0f0d24aa676fec4cfb456bdcbc0a
Mark Keranen <mark.keranen@outlook.com>
2018-02-19 16:08:56 -0500

    Update LED_Control_PWM.ino

--- LED_Control_PWM.ino
+++ LED_Control_PWM.ino
@@ -1,11 +1,13 @@
 /*
- * LED emitted intensity control by Mark Keranen
+ * LED emitted intensity control
+ * By: Mark Keranen
  * 
  * This program uses an LED controlled via PWM & a photodiode for measuring the intensity of light emitted by the LED. 
  * The user can input a desired intensity via serial communication, and the LED duty cycle will be adjusted to create 
  * the desired intensity reading at the photodiode.
  *
  * This program utilizes a PID loop control system with empirically adjusted, but un-tuned PID parameters.
+ *
  */
 
  //PID Library
